a4a5f46c41fab4516c2c6f936f6aa9392a1cf601
ppdx999 <ppdx999@gmail.com>
2024-01-16 23:28:53 +0900

    impl TRUE/FALSE/IF

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,27 @@
 import * as $ from './main.js';
 
+it('TRUE', () => {
+  const result = $.TRUE(1)(2);
+
+  expect(result).toBe(1);
+});
+
+it('FALSE', () => {
+  const result = $.FALSE(1)(2);
+
+  expect(result).toBe(2);
+});
+
+it('IF', () => {
+  const result = $.IF($.TRUE)(1)(2);
+
+  expect(result).toBe(1);
+
+  const result2 = $.IF($.FALSE)(1)(2);
+
+  expect(result2).toBe(2);
+});
+
 it('PLUS', () => {
   const Y = $.PLUS($.C2)($.C3);
 
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+export const TRUE = x => y => x;
+export const FALSE = x => y => y;
+export const IF = b => x => y => b(x)(y);
+
 export const C0 = s => z => z;
 export const C1 = s => z => s(z);
 export const C2 = s => z => s(s(z));
